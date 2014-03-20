364de24d815a470fe691cd837c0bfc57dba6097a
Joel Griffith <joel.griffith@wk.com>
2014-03-20 12:21:20 -0700

    Adding in some light docs

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 (function() {
+
     if (!Object.observe) {
+
+        /**
+         *  Observe Shim
+         */
         Object.defineProperty(Object.prototype, 'observe', {
             enumerable: false,
             configurable: true,
@@ -8,6 +13,10 @@
                 var props = Object.keys(obj),
                     length = props.length;
 
+                /**
+                 *  Interval
+                 *  This is a "private" holder for the dirty checking interval so we can clear later
+                 */
                 Object.defineProperty(obj, '__interval__', {
                     enumerable: false,
                     configurable: true,
@@ -15,6 +24,10 @@
                     value: setInterval(watchForChanges, 50)
                 });
 
+                /**
+                 *  Filter
+                 *  Abstraction for diffing array values (quicker than .filter)
+                 */
                 function filter(a, func) {
                     var match = [];
                     for (var i = 0; i < a.length; i++) {
@@ -25,6 +38,10 @@
                     return match;
                 }
 
+                /**
+                 *  Watch Property
+                 *  Given a property identifier, replace it's getter/setter so we can watch it for 'updates'
+                 */
                 function watchProperty(prop) {
                     var oldval = obj[prop],
                         newval = oldval,
@@ -54,6 +71,11 @@
                     }
                 }
 
+                /**
+                 *  Watch For Changes
+                 *  Dirty check the object's properties and handle new/deleted properties
+                 *      in the same tick, the latter ones won't be there.
+                 */
                 function watchForChanges() {
                     if (Object.keys(obj).length !== length) {
                         var newProps = Object.keys(obj),
@@ -85,6 +107,10 @@
                     }
                 }
 
+                /**
+                 *  Apply Observe
+                 *  Iterate over the array of keys on the object, and apply the getter/setter on it
+                 */
                 function applyObserve(arr) {
                     var length = arr.length;
                     for (var i = 0; i < length; i++) {
@@ -95,6 +121,10 @@
                 watchForChanges();
             }
         });
+
+        /**
+         *  Unobserve Shim
+         */
         Object.defineProperty(Object.prototype, 'unobserve', {
             enumerable: false,
             configurable: true,
@@ -106,6 +136,10 @@
                 var props = Object.keys(obj),
                     length = props.length;
 
+                /**
+                 *  Clear Observe
+                 *  Replace the custom getter/setters on a property with the static ones
+                 */
                 function clearObserve(prop) {
                     var val = obj[prop];
                     delete obj[prop];
@@ -114,6 +148,10 @@
                 for (var i = 0; i < length; i++) {
                     clearObserve(props[i]);
                 }
+
+                /**
+                 *  Clear the dirty check timer, and delete the no unused timer property
+                 */
                 clearInterval(obj.__interval__);
                 delete obj.__interval__;
             }
