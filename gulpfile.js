18e7f65b46a166892cc7ee07b98ce58e53b61f7e
Joel Griffith <joel.griffith@wk.com>
2014-03-24 16:46:31 -0700

    Watch task, code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,21 @@
-//////////////////////////////////////
-// Build Dependencies
-//
-// These are dependencies for the build
-// process, and you're free to remove
-// dependencies you're not using.
-//////////////////////////////////////
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     clean = require('gulp-clean'),
     gutil = require('gulp-util'),
     info = require('./package.json'),
     mocha = require('gulp-mocha'),
-	webpack = require('webpack');
-
-//////////////////////////////////////
-// Location Abstractions
-//
-// These are abstractions for your
-// project. Things such as build
-// directories and banner comments
-// are retrieved from these variables.
-//////////////////////////////////////
-
-	// Source Code Directory
-	var base = './',
-
-	// Dist build location to output
+	webpack = require('webpack'),
+	base = './',
 	dist = './build',
-
-	// Entry point for your JS app (builds look only at this)
 	jsIndex = './index.js',
-
-	// Package title for your comment banners in JS and CSS
 	title = info.name,
-
-	// Author title for our comment banners in JS and CSS
 	author = info.author,
-
-	// Version information for your comment banners in JS and CSS
 	version = info.version,
-
-	// Build# for your comment banners in JS and CSS
 	build = Date.now();
 
-//////////////////////////////////////////////
-// Webpack Config (JS Compiling/Modules)
-//
-// Responsbile for compiling JS. Uses
-// the Webpack build system for compilation,
-// which can build from AMD or CommonJS
-// modules.
-//////////////////////////////////////////////
+/**
+ *	Packaging Config
+ */
 var webpackConfig = {
 	cache: true,
 	entry: jsIndex,
@@ -65,27 +30,13 @@ var webpackConfig = {
 	]
 };
 
-//////////////////////////////////////
-// Task Mapping
-//
-// Gulp task abstractions happen here.
-// Feel free to move things around to
-// suite your needs.
-//////////////////////////////////////
+// Default
 gulp.task('default', ['clean'], function() {
-    gulp.start('hint', 'test', 'webpack');
+    gulp.start('hint', 'test', 'build');
 });
 
-//////////////////////////////////////
-// JavaScript Tasks
-//
-// All JavaScript tasks here. This
-// includes building Dist and Dev
-// JS and hinting as well
-//////////////////////////////////////
-
-// JS packaging for distribution
-gulp.task('webpack', function() {
+// Build Task
+gulp.task('build', function() {
 	// modify some webpack config options
 	var myConfig = Object.create(webpackConfig);
 	myConfig.output.path = dist;
@@ -119,31 +70,18 @@ gulp.task('test', function () {
         .pipe(mocha({reporter: 'nyan'}));
 });
 
-//////////////////////////////////////
-// Cleanup Tasks
-//
-// Clean will just delete all files in
-// /dev and /dist to ensure old
-// files are removed.
-//////////////////////////////////////
+// Pre-build cleanup
 gulp.task('clean', function() {
   return gulp.src([dist], {read: false})
     .pipe(clean());
 });
 
-//////////////////////////////////////
-// Watching Tasks
-//
-// Gulp watch is built here. Looks
-// at all .js and .scss files as well
-// as images. New files will require a
-// restart of the `gulp watch` command.
-//////////////////////////////////////
+// Watch task
 gulp.task('watch', function() {
 
 	// Watch .js files
-	gulp.watch(base + '*.js', function(event) {
+	gulp.watch('lib/*.js', function(event) {
 		gutil.log('Watch:', 'File ' + event.path + ' was ' + event.type + ', running tasks...');
-		gulp.start('hint', 'webpack:dev');
+		gulp.start('hint', 'build', 'test');
 	});
 });
